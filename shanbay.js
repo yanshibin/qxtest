90645f3b1816696326f883cf1ae43c9c1c61c621
Kristina <8521118@gmail.com>
2022-06-19 22:48:56 +0800

    Update shanbay.js

--- shanbay.js
+++ shanbay.js
@@ -4,17 +4,9 @@ const adold = 'getGlobalData';
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) != -1){
-      obj.objects[0]["due_date"] = "2029-06-14";
-      obj.objects[1]["due_date"] = "2029-06-14";
-      obj.objects[2]["due_date"] = "2029-06-14";
-      obj.objects[3]["due_date"] = "2029-06-14";
-      obj.objects[4]["due_date"] = "2029-06-14";
-      obj.objects[0]["status"] = "IN_USE";
-      obj.objects[1]["status"] = "IN_USE";
-      obj.objects[2]["status"] = "IN_USE";
-      obj.objects[3]["status"] = "IN_USE";
-      obj.objects[4]["status"] = "IN_USE";
-}
+      obj.objects.forEach(function(item,index){
+      item.due_date="2023-06-15"
+})
 
 if ($request.url.indexOf(vip) != -1) {
       obj["status"] = "WITHIN_PERIOD";
